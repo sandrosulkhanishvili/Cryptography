4e59137b40a735cb98f99e583afa455a32323bc1
Sandro Sulkhanishvili <sandro.sulkhanishvili@gmail.com>
2023-06-08 01:05:37 +0400

    V 0,5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { EncryptionComponent } from './encryption/encryption.component';
+import { DecryptionComponent } from './decryption/decryption.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, EncryptionComponent, DecryptionComponent],
+  imports: [FormsModule, BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/decryption/decryption.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+
+@Component({
+  selector: 'app-decryption',
+  templateUrl: './decryption.component.html',
+  styleUrls: ['./decryption.component.scss'],
+})
+export class DecryptionComponent implements OnInit {
+  public selectedOption:
+    | 'AES'
+    | 'DES'
+    | 'Triple DES'
+    | 'Rabbit'
+    | 'RC4'
+    | 'Option5' = 'AES';
+  public inputData: string = '';
+  public password: string = null;
+  public outputData: any = '';
+  public types: string[] = [
+    'AES',
+    'DES',
+    'Triple DES',
+    'Rabbit',
+    'RC4',
+    'Option5',
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onDecrypt() {
+    console.log('this.outputData :', this.outputData);
+    switch (this.selectedOption) {
+      case 'AES':
+        this.outputData = CryptoJS.AES.decrypt(
+          this.inputData,
+          this.password
+        ).toString(CryptoJS.enc.Utf8);
+        break;
+      case 'DES':
+        this.outputData = CryptoJS.DES.decrypt(
+          this.inputData,
+          this.password
+        ).toString(CryptoJS.enc.Utf8);
+        break;
+      case 'Triple DES':
+        this.outputData = CryptoJS.TripleDES.decrypt(
+          this.inputData,
+          this.password
+        ).toString(CryptoJS.enc.Utf8);
+        break;
+      case 'Rabbit':
+        this.outputData = CryptoJS.Rabbit.decrypt(
+          this.inputData,
+          this.password
+        ).toString(CryptoJS.enc.Utf8);
+        break;
+      case 'RC4':
+        this.outputData = CryptoJS.RC4.decrypt(
+          this.inputData,
+          this.password
+        ).toString(CryptoJS.enc.Utf8);
+        break;
+      case 'Option5':
+        console.log(this.selectedOption);
+        console.log(this.selectedOption);
+        break;
+    }
+  }
+}
